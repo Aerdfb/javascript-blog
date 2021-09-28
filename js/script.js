ae82d24b1c657030b25bc8cef35ba5796c13955e
Dominik Krawczyk <dominix.kr@gmail.com>
2021-09-28 19:10:24 +0200

    add class 'active' to current element

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const titleClickHandler = function(){
     console.log('Link was clicked!');
     console.log(event)
 
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -16,9 +16,14 @@ const titleClickHandler = function(){
       activeLink.classList.remove('active');
     }
 
-    /* add class 'active' to the clicked link */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+
+    const clickedElement = this;
+    console.log('clickedElement (with plus): ' + clickedElement);
+
+    clickedElement.classList.add('active');
     
-    /* remove class 'active' from all articles */
+    /* [DONE] remove class 'active' from all articles */
 
     const activeArticles = document.querySelectorAll('.posts article.active');
 
@@ -38,4 +43,5 @@ const titleClickHandler = function(){
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
+
 
